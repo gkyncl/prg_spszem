50110b60f177e4919ddf0a32247cd15ba96b676e
gkyncl <56367707+gkyncl@users.noreply.github.com>
2023-04-13 17:56:25 +0200

    skripty k lekci 8

---
+++ lekce_8/prg_8a.py
@@ -0,0 +1,21 @@
+# prg_8_a - cteni a zapis ze/do souboru
+
+# nacteni souboru
+# vypis po radcich
+# zapis do souboru
+
+with open(...) as file:
+    for ... in ...:
+
+
+
+
+with open(...) as f:
+    f.write("\n")
+    ...
+
+
+
+
+
+
---
+++ lekce_8/prg_8b.py
@@ -0,0 +1,14 @@
+# prg_8_a - tvorba programu pro spravu uzivatelu (jednoducha verze)
+
+# vytvorte program, ktery bude simulovat system pro spravu uzivatelu
+# pri spusteni se program zepta jestli chceme registrovat noveho uzivatele nebo se prihlasit
+# pri volbe registrace zada uzivatel uzivatelske jmeno a heslo, ktere musi splnovat vybrana kriteria
+# metody nad instanci string: (https://www.w3schools.com/python/python_ref_string.asp)
+# uzivatele se ukladaji do seznamu uzivatelu - vzdy jako seznam dvou hodnot
+
+# pri volbe prihlaseni je uzivatel tazan na uz. jmeno
+# pokud dane uziv. jmeno existuje v seznamu, je uzivatel pozadan o heslo na jehoz zadani ma 3 pokusy
+# pokud zada spravne, vypise se hlaska, ze registrace probehla uspesne
+# pokud zada 3x spatne, program skonci a da o tom info
+
+# mozne vylepseni - uzivatele se zapisuji do souboru a ne do seznamu - seznam je trvaly
