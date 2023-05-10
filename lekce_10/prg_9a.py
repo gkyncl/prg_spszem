f7400e0dcaa3193ce4fafa2e95683aeae9c9977f
JaSi <jasi@atlasltd.cz>
2023-05-10 14:32:28 +0200

    10. cviceni

---
+++ lekce_10/prg_9a.py
@@ -0,0 +1,12 @@
+# knihovna matplotlib
+# https://matplotlib.org/
+# knihovna pro kresleni grafu ve 2d i ve 3d
+
+
+# napiste kod, ktery vykresli:
+# usecky - ruzne typy car
+# body - ruzne typy bodu
+# funkce sinus, cosinus...
+# kruznici danou stredem a polomerem
+# histogram
+# zapisnik mereni
